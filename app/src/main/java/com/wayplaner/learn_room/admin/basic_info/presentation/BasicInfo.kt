6f26502e0a1ebc0a7f848d3a22f8661b27306d83
FOXesies <n.cryazhev@yandex.ru>
2024-05-11 17:58:55 +0300

    design admin init

---
+++ app/src/main/java/com/wayplaner/learn_room/admin/basic_info/presentation/BasicInfo.kt
@@ -0,0 +1,96 @@
+package com.wayplaner.learn_room.admin.basic_info.presentation
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.wayplaner.learn_room.R
+import com.wayplaner.learn_room.ui.theme.lightGrayColor
+import com.wayplaner.learn_room.ui.theme.redBlackColor
+import com.wayplaner.learn_room.ui.theme.redLogoColor
+
+@Composable
+fun BasicInfo() {
+    val colorET = TextFieldDefaults.colors(
+        focusedIndicatorColor = Color.Transparent,
+        unfocusedIndicatorColor = Color.Transparent,
+        disabledContainerColor = lightGrayColor,
+        focusedContainerColor = lightGrayColor,
+        unfocusedContainerColor = lightGrayColor,
+    )
+
+    Column(modifier = Modifier.fillMaxSize()) {
+        Image(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(220.dp),
+            contentScale = ContentScale.Crop,
+            painter = painterResource(id = R.drawable.burger_king),
+            contentDescription = ""
+        )
+
+        Spacer(modifier = Modifier.height(30.dp))
+
+        Column(modifier = Modifier.padding(horizontal = 30.dp)) {
+
+            Text(text = "Назввание ресторана", fontSize = 16.sp,
+                modifier = Modifier.padding(start = 5.dp))
+
+            Spacer(modifier = Modifier.height(2.dp))
+
+            TextField(value = "Бургер кинг",
+                onValueChange = {},
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clip(RoundedCornerShape(20)),
+                colors = colorET)
+
+            Spacer(modifier = Modifier.height(20.dp))
+
+            Button(onClick = { /*TODO*/ },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(45.dp),
+                colors = ButtonDefaults.buttonColors(redLogoColor),
+                shape = RoundedCornerShape(40)
+            ) {
+                Text(text = "Изменить фото")
+            }
+        }
+
+        Spacer(modifier = Modifier.height(30.dp))
+
+        Button(onClick = { /*TODO*/ },
+            Modifier.padding(horizontal = 30.dp).fillMaxWidth().height(45.dp),
+            colors = ButtonDefaults.buttonColors(redBlackColor),
+            shape = RoundedCornerShape(40)
+        ) {
+            Text(text = "Сохранить изменения")
+        }
+    }
+}
+
+
+@Composable
+@Preview(showBackground = true, showSystemUi = true)
+fun BasicInfof(){
+    BasicInfo()
+}
---
+++ app/src/main/java/com/wayplaner/learn_room/admin/home/presentation/HomeScreen.kt
@@ -0,0 +1,83 @@
+package com.wayplaner.learn_room.admin.home.presentation
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.wayplaner.learn_room.ui.theme.redLogoColor
+
+@Composable
+fun HomeScreen() {
+    Column(modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,){
+        Column(
+            modifier = Modifier
+                .wrapContentWidth()
+                .align(Alignment.CenterHorizontally)
+        ) {
+            Button(onClick = { /*TODO*/ },
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .width(250.dp),
+                colors = ButtonDefaults.buttonColors(redLogoColor),
+                shape = RoundedCornerShape(40)
+            ) {
+                Text(text = "Основная информация")
+            }
+
+            Spacer(modifier = Modifier.height(20.dp))
+
+            Button(onClick = { /*TODO*/ },
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .width(250.dp),
+                colors = ButtonDefaults.buttonColors(redLogoColor),
+                shape = RoundedCornerShape(40)
+            ) {
+                Text(text = "Меню")
+            }
+
+            Spacer(modifier = Modifier.height(20.dp))
+
+            Button(onClick = { /*TODO*/ },
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .width(250.dp),
+                colors = ButtonDefaults.buttonColors(redLogoColor),
+                shape = RoundedCornerShape(40)
+            ) {
+                Text(text = "Отзывы")
+            }
+
+            Spacer(modifier = Modifier.height(20.dp))
+
+            Button(onClick = { /*TODO*/ },
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .width(250.dp),
+                colors = ButtonDefaults.buttonColors(redLogoColor),
+                shape = RoundedCornerShape(40)
+            ) {
+                Text(text = "Настройки")
+            }
+        }
+    }
+}
+
+@Composable
+@Preview(showBackground = true, showSystemUi = true)
+fun fsdf(){
+    HomeScreen()
+}
